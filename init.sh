133c585b8adc19e06b4238cc16aafa388c0d075b
Ting Young <ting.young@maxwit.com>
2018-04-29 01:03:51 +0800

    update
    Signed-off-by: Ting Young <ting.young@maxwit.com>

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 # add content
-sudo ldapadd -x -D "cn=admin,dc=maxwit,dc=com" -w "maxwit" -f ldif/add_content.ldif
+sudo ldapadd -x -D "cn=admin,dc=maxwit,dc=com" -w "maxwit" -f ldif/organization/add_organizations.ldif
+sudo ldapadd -x -D "cn=admin,dc=maxwit,dc=com" -w "maxwit" -f ldif/group/add_groups.ldif
+sudo ldapadd -x -D "cn=admin,dc=maxwit,dc=com" -w "maxwit" -f ldif/user/add_users.ldif
 
 # search content
 sudo ldapsearch -x -b "dc=maxwit,dc=com" -D "cn=admin,dc=maxwit,dc=com" -w "maxwit"
